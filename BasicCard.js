90b02b56f45b9c4196ac6eeecd9e291eb490607b
Whiting1 <11jhsballfpw@gmail.com>
2017-07-28 00:17:24 -0400

    second commit

---
+++ BasicCard.js
@@ -0,0 +1,11 @@
+module.exports = BasicCard;
+
+//front & back needs to have proprty for text of front and back
+
+var Frontcard = function (front, back) {
+
+	this.front = front;
+	this.back = back;
+
+};
+
---
+++ ClozeCard.js
@@ -0,0 +1,4 @@
+module.exports = ClozeCard;]
+
+
+var Frontcard = function (text, cloze) {
